e32898acfc559f4365b88f3d5fd10fee44e55b81
Михаил Евтеев <mevteev@npv.su>
2020-11-12 17:35:46 +0300

    создание ViewModel

---
+++ app/src/main/java/com/example/noteprojectkotlin/MainViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.noteprojectkotlin
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    private val viewStateLiveData: MutableLiveData<String> = MutableLiveData() // private val viewStateLiveDate = MutableLiveData<String>
+    
+
+}
